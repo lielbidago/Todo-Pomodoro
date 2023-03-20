97330ed1b3f8012a90907cf4f8beb93efa7da7ce
lielbidago <liloplev@gmail.com>
2023-03-20 19:13:35 +0200

    added sound to timer

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import {customeBackground} from '../helperFunctions/themes'
 
+
+
 export function WelcomePage(){
-    
+
     
     return (
         <div className="welcome-page" style={customeBackground()}>
@@ -11,7 +13,9 @@ export function WelcomePage(){
                 <h1>Todo Pomodoro!</h1>
                 <p>Here you can enter your tasks into a todo list and create a timeline for compilation,<br/>
                      that you'll complete using our pomodoro timer</p>
+                     
                 <Link role="button" type="button" className="start btn btn-outline-dark" to={`/Todos-and-pomodoro`}>START</Link>
+
             </div>
         </div>
     )
